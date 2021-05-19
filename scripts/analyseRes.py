bca2f966daddac52174c3e5ee4aa01dd6aa1a1a4
sunxunxunxun <sunxun1116@foxmail.com>
2021-05-19 09:25:54 +0800

    analyse results

--- scripts/analyseRes.py
+++ scripts/analyseRes.py
@@ -1,4 +1,4 @@
-import os, json
+import os, json, ast
 import variables
 
 resDir = "/home/sunxun/data/results/"
@@ -7,8 +7,63 @@ def analyseOyente():
     resD =  resDir + "res_oyente/"
     resFls = os.listdir(resD)
     outfile = resDir + "oyente.txt"
+    wf = open(outfile, 'w', encoding='utf-8')
+    # print(len(resFls))
     for fl in resFls:
-        res = json.load(open(resD + fl, 'r', encoding='utf-8').read())
-        print(res)
-        vul = [0,0,0,0,0,0,0]
+        print(fl)
+        res = json.load(open(resD + fl, 'r', encoding='utf-8'))
+        # print(res, res['vulnerabilities'])
+        # print(type(res))
+        vul = [0 for _ in range(7)]  
+        vul[variables.OYNETE['evm_code_coverage']] = res['evm_code_coverage']
+        vul[variables.OYNETE['callstack']] = int(res['vulnerabilities']['callstack'])
+        vul[variables.OYNETE['reentrancy']] = int(res['vulnerabilities']['reentrancy'])
+        vul[variables.OYNETE['integer_overflow']] = len(res['vulnerabilities']['integer_overflow'])
+        vul[variables.OYNETE['integer_underflow']] = len(res['vulnerabilities']['integer_underflow'])
+        vul[variables.OYNETE['time_dependency']] = int(res['vulnerabilities']['time_dependency'])
+        vul[variables.OYNETE['money_concurrency']] = int(res['vulnerabilities']['money_concurrency'])
+        
+        
+        vul = [str(_) for _ in vul]
+        addr = fl.split('.')[0]
+        # print(addr + '#' + '#'.join(vul))
+        wf.write(addr + '#' + '#'.join(vul) + '\n')
+    wf.close()
 
+def analyseOsiris():
+    resD = resDir + 'res_osiris/'
+    resFls = os.listdir(resD)
+    outfile = resDir + 'osiris.txt'
+    wf = open(outfile, 'w', encoding='utf-8')
+    
+    for fl in resFls:
+        print(fl)
+        res = json.load(open(resD + fl, 'r', encoding='utf-8'))
+        # print(res)
+        vul = [0 for i in range(15)]    # 
+        # print(vul)
+        for k, v in res.items():
+            if k not in variables.OSIRIS:
+                continue
+            # print(k, v)
+            if type(v) is bool:
+                vul[variables.OSIRIS[k]] = int(v)
+            elif v.find('Opcode') > 0:
+                # print(v)
+                vul[variables.OSIRIS[k]] = v.count('Opcode')
+                # print(vul[variables.OSIRIS[k]], variables.OSIRIS[k])
+            else:  
+                vul[variables.OSIRIS[k]] = v
+        
+        addr = fl.split('.')[0]
+        # print(addr, vul)
+        vul = [str(_) for _ in vul]    
+        print(addr + '#' + '#'.join(vul))
+        wf.write(addr + '#' + '#'.join(vul) + '\n')
+    
+    wf.close()
+
+
+if __name__ == "__main__":
+    analyseOyente()
+    # analyseOsiris()
--- scripts/variables.py
+++ scripts/variables.py
@@ -1,9 +1,28 @@
-OYENTE = {
-    'Coverge': 0
-    'Callstack': 1
-    'Reentrancy': 2
-    'Time_dependency': 3
-    'Overflow': 4
-    'Underflow': 5
-    'Money_concurrency': 6
+OYNETE = {
+    'evm_code_coverage': 0,    # float
+    'callstack': 1,  # bool
+    'reentrancy': 2, # bool
+    'integer_overflow': 3,   # int
+    'integer_underflow': 4,  # int
+    'time_dependency': 5,    # bool
+    'money_concurrency': 6,  # bool
 }
+
+OSIRIS = {
+    'evm_code_coverage': 0,    # float
+    'callstack': 1,  # bool
+    'reentrancy': 2, # bool
+    'overflow': 3,   # int
+    'underflow': 4,  # int
+    'time_dependency': 5,    # bool
+    'money_concurrency': 6,  # bool
+    'timeout': 7,   # bool
+    'division': 8,  # int
+    'execution_time': 9,    # float
+    'signedness': 10,   # int
+    'assertion_failure': 11,    # bool
+    'modulo': 12,   # bool
+    'execution_paths': 13,  # int
+    'truncation': 14,
+}
+
